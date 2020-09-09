9c6a0e453ff3d4eceb1f86842c266dc8fc5595c1
Piyawut Sinchavanwat <piyawut.c@codium.co>
2020-09-09 13:30:01 +0700

    Optimize code

--- main.ts
+++ main.ts
@@ -1,32 +1,9 @@
-export function updateGameScore(player1Points: number, player2Points: number): Game {
-    return new Game(player1Points, player2Points)
+export function updateGameScore(player1Score: number, player2Score: number): Game {
+    return new Game(player1Score, player2Score)
 }
 
 export function decideGameWinner(game: Game): string {
-    const player1Points = game.player1Points
-    const player2Points = game.player2Points
-    
-    if (player1Points>= 3 && player2Points>=3) {
-        // deuce
-        if (player1Points - player2Points >= 2){
-            return 'Player 1'
-        } else if (player2Points - player1Points >= 2){
-            return 'Player 2'
-        } else {
-            return 'Unfinish'
-        }
-
-    } else {
-        //not deuce
-        if (player1Points==4 && player2Points < 3){
-            return 'Player 1'
-        } else if (player2Points==4 && player1Points < 3) {
-            return 'Player 2'
-        } else {
-            return 'Wrong score'
-        }
-    }
-    return 'Error'
+    return game.decideGameWinner()
 }
 
 export function printScore(scoreArray: Array<number>): string {
@@ -50,12 +27,72 @@ export function printScore(scoreArray: Array<number>): string {
     return 'Error'
 }
 
-class Game {
-    public player1Points: number
-    public player2Points: number
+class Player {
+    public score: number;
 
-    constructor(player1Points: number, player2Points: number){
-        this.player1Points = player1Points
-        this.player2Points = player2Points
+    constructor(score: number) {
+        this.score = score
+    }
+
+    getScore(): number {
+        return this.score
+    }
+
+    setScore(score: number) {
+        this.score = score
+    }
+
+    isMoreThan(score: number): boolean {
+        return this.score > score
+    }
+
+    canDeuce(): boolean {
+        return this.score > 2
+    }
+}
+
+
+class Game {
+    public player1: Player
+    public player2: Player
+
+    constructor(player1Score: number, player2Score: number) {
+        this.player1 = new Player(player1Score)
+        this.player2 = new Player(player2Score)
+    }
+
+    getPlayerScore(): [number, number] {
+        return [this.player1.getScore(), this.player2.getScore()]
+    }
+
+    setPlayerScore(player1Score: number, player2Score: number) {
+        if (player1Score) {
+            this.player1.setScore(player1Score)
+        }
+        if (player2Score) {
+            this.player2.setScore(player2Score)
+        }
+    }
+
+    decideGameWinner(): string {
+
+        if (this.player1.canDeuce() && this.player2.canDeuce()) {
+            if (this.player1.getScore() - this.player2.getScore() > 1){
+                return 'Player 1'
+            } else if (this.player2.getScore() - this.player1.getScore() > 1){
+                return 'Player 2'
+            } else {
+                return 'Unfinish'
+            }
+        } else {
+            if (this.player1.getScore()==4 && this.player2.getScore() < 3){
+                return 'Player 1'
+            } else if (this.player2.getScore()==4 && this.player1.getScore() < 3) {
+                return 'Player 2'
+            } else {
+                return 'Wrong Score'
+            }
+        }
+        return 'Error'
     }
 }
